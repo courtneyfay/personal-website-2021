67888c6cf55467ef2581bc1561faf007f82ba6cd
Courtney Fay <fay.courtney@gmail.com>
2021-07-22 10:54:26 -0600

    Adding blog posts to site

--- public/main.js
+++ public/main.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const directoryPath = path.join(__dirname, "../src/posts");
 let postList = [];
 
-const getPosts = async () => {
-    await fs.readdir(directoryPath, (err, files) => {
+const getPosts = () => {
+    fs.readdir(directoryPath, (err, files) => {
         if (err) {
             return console.log("Failed to list contents of directory: " + err);
         }
@@ -14,9 +14,9 @@ const getPosts = async () => {
             let post;
             fs.readFile(`${directoryPath}/${file}`, 'utf8', (err, contents) => {
                 const lines = contents.split("\n");
-                const getMetadataIndices = (accumulator, element, index) => {
+                const getMetadataIndices = (accumulator, element, i) => {
                     if (/^---/.test(element)) {
-                        accumulator.push(index)
+                        accumulator.push(i)
                     }
                     return accumulator;
                 };
@@ -46,12 +46,14 @@ const getPosts = async () => {
                     content: content ? content : "No content given",
                 }
                 postList.push(post);
+                if (index === files.length - 2) {
+                    let data = JSON.stringify(postList);
+                    fs.writeFileSync("src/posts.json", data)
+                }
             });
         })
     });
-    setTimeout(() => {
-        console.log('postList', postList)
-    }, 500)
+    return;
 }
 
 getPosts();
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import SectionHeader from './SectionHeader';
+import posts from '../posts.json';
 
 const Blog = () => {
     return (
-        <SectionHeader
-            text='blog'
-        />
+        <>
+            <SectionHeader
+                text='blog'
+            />
+            {posts && posts.map(post => {
+                return (
+                    <>
+                        <div>{post.title}</div>
+                        <div>{post.id}</div>
+                        <div>{post.date}</div>
+                        <div>{post.tags.toString()}</div>
+                        <div>{post.content}</div>
+                    </>
+                )
+            })
+            }
+        </>
     )
 };
 
